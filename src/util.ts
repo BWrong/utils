6c88edebe16c0a50f861352c24fdd0e15fd4e7cc
bwrong <ibwrong@foxmail.com>
2021-11-22 15:56:35 +0800

    fix: 优化算法

--- src/util.ts
+++ src/util.ts
@@ -8,16 +8,26 @@
  * @param {*} pidName    父级标识key
  * @param {*} idName     id标识key
  */
-export function convertToTree({ data = [], pid = 0, children = 'children', pidName = 'parentId', idName = 'id' }) {
-  let arr:any[] = [];
-  data.map((item:any) => {
-    if (item[pidName] === pid) {
-      let child:any[] = item[children] || [];
-      item[children] = child.concat(convertToTree({ data, pid: item[idName], children, pidName, idName }));
-      arr.push(item);
+ interface _ITreeData {
+  data: any[];
+  pid?: number | string;
+  children?: string;
+  pidName?: string;
+  idName?: string;
+}
+export function convertToTree({ data = [], pid = 0, children = 'children', pidName = 'parentId', idName = 'id' }:_ITreeData) {
+  const tree:any[] = [],
+    map:Record<string,any> = {};
+  data.forEach((item) => {
+    item[children] = map[item[idName]] = map[item[idName]] || [];
+    if (item[pidName]) {
+      map[item[pidName]] = map[item[pidName]] || [];
+      map[item[pidName]].push(item);
+    } else {
+      tree.push(item);
     }
   });
-  return arr;
+  return pid ? map[pid] : tree;
 }
 /**
  * 从数组中根据id获取所有父级元素，返回数组
@@ -29,13 +39,13 @@ export function convertToTree({ data = [], pid = 0, children = 'children', pidNa
  * @param result // 返回结果
  */
 interface _IParams {
-  id: string,
-  data: any[],
-  idName?: string,
-  pidName?: string,
-  result?: object[]
+  id: string;
+  data: any[];
+  idName?: string;
+  pidName?: string;
+  result?: object[];
 }
-export function getParentsFromArray({ id, data, idName = 'id', pidName = 'parentId', result = [] }:_IParams) {
+export function getParentsFromArray({ id, data, idName = 'id', pidName = 'parentId', result = [] }: _IParams) {
   for (const item of data) {
     if (item[idName] === id) {
       result.unshift(item);
